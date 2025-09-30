de2adfe74cb7d928d90eac7e1b415dd5fb61a403
sebha01 <30077548@students.southwales.ac.uk>
2025-09-30 12:30:26 +0100

    typing out more questions and setting some stuff up

--- 30077548IntelligentSystemsAssignment1/Fact.h
+++ 30077548IntelligentSystemsAssignment1/Fact.h
@@ -3,6 +3,15 @@
 
 class Fact {
 public:
+	//member variables
 	std::string factName;
 	std::string factValue;
+
+	//member functions
+	void setFactValue(std::string f);
 };
+
+void Fact::setFactValue(std::string f) 
+{
+	this->factValue = f;
+}
--- 30077548IntelligentSystemsAssignment1/GlobalHeader.h
+++ 30077548IntelligentSystemsAssignment1/GlobalHeader.h
@@ -9,3 +9,17 @@ enum Decision {
 	NO,
 	YES
 };
+
+std::vector<std::string> questionsToAsk = 
+{
+	"How many customers are in the restaurant?\n1| A few\n2| Some\n3| Full of customers\n",
+	"How long is the waiting time before being seated?\n1| More than 60 minutes\n2| Between 31 and 60 minutes\n3| Between 10 and 30 minutes?\n4| Less than 10 minutes\n",
+	"Are you hungry?\n1| Yes\n2| No\n",
+	"Are there alternative restaurants you can go to nearby?\n1| Yes\n2| No\n"
+	"Do you have a reservation with the restaurant?\n1| Yes\n2| No\n",
+	"",
+	"",
+	"",
+	"",
+	"",
+};
