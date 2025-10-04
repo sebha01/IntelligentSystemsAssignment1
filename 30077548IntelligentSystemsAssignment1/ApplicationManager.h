a5737ea1e7a614093240f8a185d497fd47421a42
sebha01 <30077548@students.southwales.ac.uk>
2025-10-04 23:37:50 +0100

    Cleared up application manager file

--- 30077548IntelligentSystemsAssignment1/ApplicationManager.h
+++ 30077548IntelligentSystemsAssignment1/ApplicationManager.h
@@ -12,11 +12,7 @@ public:
 	void init();
 	void showTitle();
 	void update();
-	int validateInput(int minChoice, int maxChoice);
 	int resetChoice();
-	void startDelay(double secondsToDelay, std::string messageToDisplay);
-	void clearScreen();
-	void endScreen();
 };
 
 void ApplicationManager::init()
@@ -48,44 +44,4 @@ void ApplicationManager::update()
 
 	//Exit now that everything needed has been completed
 	exit(0);
-}
-
-
-
-void ApplicationManager::startDelay(double secondsToDelay, std::string messageToDisplay)
-{
-	//Timer splits into 3 and adds a . to the screen then waits a quarter of the time passed in before moving on
-	double third = secondsToDelay / 3;
-	double quarter = secondsToDelay / 4;
-
-	//display loading message or whatever is passed in
-	std::cout << messageToDisplay;
-
-	for (int i = 0; i < 3; i++)
-	{
-		//idea of the timer obtained from https://www.geeksforgeeks.org/how-to-add-timed-delay-in-cpp/ , then implemented it for microseconds so i could use a double value
-		std::this_thread::sleep_for(std::chrono::milliseconds(static_cast<int>(third * 1000)));
-		std::cout << ".";
-	}
-
-	//last touch so user can see it has finished loading
-	std::this_thread::sleep_for(std::chrono::milliseconds(static_cast<int>(quarter * 1000)));
-	std::cout << std::endl;
-}
-
-void ApplicationManager::clearScreen()
-{
-	//Clear the screen and show the title at the top before next set of outputs are displayed
-	system("cls");
-	showTitle();
-}
-
-void ApplicationManager::endScreen()
-{
-	//Show the end screen after order tracking complete before moving back to the main menu
-	std::cout << std::endl << "Enjoy your order, have a lovely day and thank you for using Hot Eats today :)" << std::endl;
-	std::cout << "Goodbye!" << std::endl;
-
-	startDelay(8.0, "Returning to main menu");
-	clearScreen();
 }
--- 30077548IntelligentSystemsAssignment1/GlobalHeader.h
+++ 30077548IntelligentSystemsAssignment1/GlobalHeader.h
@@ -1,15 +1,9 @@
 #pragma once
 
 #include <iostream>
-#include <cstdlib>
-#include <windows.h>
 #include <iomanip>
-#include <chrono>
-#include <thread>
 #include <vector>
-#include <fstream>
 #include <string>
-#include <utility>
 #include <algorithm>
 
 enum Decision {
